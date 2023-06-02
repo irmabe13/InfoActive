6916e265c64eff254af7150161f7d4f4220df803
TheoM <mousniert.th@gmail.com>
2023-06-02 13:06:29 +0200

    programmme maquette du site

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+        <head>
+          <title>Info Activ</title>
+          <link href="style.css" rel="stylesheet">
+        </head>
+    <body>
+        <div class="container">
+          <header>
+            <div class="logo">
+              <img src="image/lycée%20la%20coliniere.jpeg" alt="logo du lycée la coliniere">
+            </div>
+            <h1>Info Activ</h1>
+            <nav>
+              <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Équipe</a></li>
+              </ul>
+            </nav>
+          </header>
+            <div class="content">
+            <section class="presentation">
+                <div class="left-block">
+                    <!-- Contenu du bloc à gauche (Présentation de l'association Info Activ) -->
+                    L'associationInfo Activ c'est une association du Lycée la coliniere pour les BTS ...
+                </div>
+
+                <div class="actions">
+
+                    <div class="arrow-up">
+                        <!-- Flèche vers le haut pour voir les actions des années précédentes -->
+                    </div>
+                    <div class="arrow-down">
+                        <!-- Flèche vers le bas pour voir les actions des années suivantes -->
+                    </div>
+                </div>
+            </section>
+
+            <section class="partnership">
+                <div class="left-block">
+                    <p>Nantes Metropole</p>
+                </div>
+                <div class="middle-block">
+                    <!-- Contenu du bloc d'écran moyen -->
+                </div>
+            </section>
+            </div>
+            <footer>
+            <div class="footer-links">
+              <a href="https://coliniere.paysdelaloire.e-lyco.fr/">Lycée la Coliniere</a>
+              <a href="https://www.linkedin.com/company/lycee-general-et-technologique-la-coliniere/">LinkedIn du lycée</a>
+              <a href="#">Mentions légales</a>
+            </div>
+            <div class="contact-info">
+              <p>Adresse : 129 Rue du Landreau, 44300 Nantes</p>
+              <p>Téléphone : 02 40 93 93 33</p>
+              <p>Email : infoactiv@example.com</p>
+            </div>
+          </footer>
+
+        </div>
+    </body>
+</html>
