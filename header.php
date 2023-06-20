bf74ca6cd8781b76d72996b9c2f7c906ae7f85aa
winniga <dkoudjowa@gmail.com>
2023-06-20 09:21:42 +0200

    ci: header modif mise en forme header

--- header.php
+++ header.php
@@ -2,44 +2,54 @@
 
 
 <header role="banner">
+<div class="container-header">
+<div class="center ptm tc"><a href="<?=$GLOBALS['home']?>"><?php media('logo', '320')?></a></div>
+<nav role="navigation" class="mtm mbm tc" aria-label="<?php _e("Browsing menu")?>">
 
-	<section class="mw960p mod center relative">
+	<button type="button" class="burger" aria-expanded="false" aria-controls="main-navigation">
+		<span class="open">Menu</span>
+		<span class="close none"><?php _e("Close")?></span>
+	</button>
+	
+	<ul id="main-navigation" class="grid up">
+		<?php
+		// Extraction du menu
+		foreach($GLOBALS['nav'] as $cle => $val)
+		{
+			// Menu sélectionné si page en cours ou article (actu)
+			if(get_url() == $val['href'] or (@$res['type'] == "article" and $val['href'] == "actualites"))
+				$selected = " selected";
+			else
+				$selected = "";
 
-		<div>
-			<nav role="navigation" aria-label="<?php _e("Quick access")?>" class="inline mlt"><a href="#main" class="acces-rapide"><?php _e("Skip to content")?></a></nav>
-			|
-			<input type="checkbox" name="high-contrast" id="high-contrast"<?=(@$_COOKIE['high-contrast']?'checked="checked"':'')?>> <label for="high-contrast"><?php _e("Enhanced contrast")?></label>
-		</div>
+			echo"<li><a href=\"".make_url($val['href'], array("domaine" => true))."\"".($val['id']?" id='".$val['id']."'":"")."".($val['target']?" target='".$val['target']."'":"")." class='".$selected."'".($selected?' title="'.$val['text'].' - '.__("current page").'"':'').">".$val['text']."</a></li>";
+		}
+		?>
+	</ul>
 
-		<div class="center ptm tc"><a href="<?=$GLOBALS['home']?>"><?php media('logo', '320')?></a></div>
+	</nav>
 
 
-		<nav role="navigation" class="mtm mbm tc" aria-label="<?php _e("Browsing menu")?>">
+		
+	
+    
+	
+</div>
 
-			<button type="button" class="burger" aria-expanded="false" aria-controls="main-navigation">
-				<span class="open">Menu</span>
-				<span class="close none"><?php _e("Close")?></span>
-			</button>
-			
-			<ul id="main-navigation" class="grid up">
-				<?php
-				// Extraction du menu
-				foreach($GLOBALS['nav'] as $cle => $val)
-				{
-					// Menu sélectionné si page en cours ou article (actu)
-					if(get_url() == $val['href'] or (@$res['type'] == "article" and $val['href'] == "actualites"))
-						$selected = " selected";
-					else
-						$selected = "";
+	
+<section class="mw960p mod center relative">
 
-					echo"<li><a href=\"".make_url($val['href'], array("domaine" => true))."\"".($val['id']?" id='".$val['id']."'":"")."".($val['target']?" target='".$val['target']."'":"")." class='".$selected."'".($selected?' title="'.$val['text'].' - '.__("current page").'"':'').">".$val['text']."</a></li>";
-				}
-				?>
-			</ul>
 
-		</nav>
+	<div>
+		<nav role="navigation" aria-label="<?php _e("Quick access")?>" class="inline mlt"><a href="#main" class="acces-rapide"><?php _e("Skip to content")?></a></nav>
+	|
+		<input type="checkbox" name="high-contrast" id="high-contrast"<?=(@$_COOKIE['high-contrast']?'checked="checked"':'')?>> <label for="high-contrast"><?php _e("Enhanced contrast")?></label>
+	</div>
 
+	
+
+
+	
 
 	</section>
-
 </header>
