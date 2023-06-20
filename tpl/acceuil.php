121d3392e5b7551e5c77606629368ec3953c9f80
winniga <dkoudjowa@gmail.com>
2023-06-20 15:27:20 +0200

    feat(acceuil): mise en place blocs éditables
    Remplacement des textes en dur par des blocs éditables
    
    Référence: stage/infoactive#26

---
+++ tpl/acceuil.php
@@ -0,0 +1,15 @@
+<?php if(!$GLOBALS['domain']) exit;?>
+<div class="container-acceuil">
+    <h2>Qui sommes-nous ?</h2>
+    <article class="pal ptm">
+
+        <?php txt('texte')?>
+
+    </article>
+
+    <a href="http://infoactiv.local/a-propos"> <button class="button1">En savoir plus</button></a>
+    <a href="http://infoactiv.local/blog"><h3>Dernières actus</h3></a>
+
+
+
+</div>
